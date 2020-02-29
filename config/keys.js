aee5b734faedee24437e0f7442fd50b4644d46d4
Roderick Trice <rocky_trice@yahoo.com>
2020-02-29 02:39:05 -0500

    init commit. Adding configuring node server and database connection

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://dbRocky:Isabel2019@cluster0-nsl8n.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
---
+++ server.js
@@ -0,0 +1,46 @@
+// Require our dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const http = require("http");
+
+//import routes
+const userRoutes = require('./routes/user')
+
+// Instantiate our Express App
+const app = express();
+
+/// middleware
+// 🤦🏾‍♂️remember the order in which you declare routes/middleware!!! if the routes are declared before the bodyparser middleware (which is used to populate req.body), body-parser won't get called for request starting with /api 😅
+// <=============parse incoming request into json=======================>
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// db config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log("🗄  MongoDB Connected"))
+    .catch(err => console.log(err));
+
+// App setup=================================================================
+// middleware (morgan-logging framwork)
+app.use(morgan("combined"));
+app.use(cors());
+
+//routes middleware
+app.use("/api", userRoutes);
+// Server Setup
+const PORT = process.env.PORT || 3001;
+const server = http.createServer(app);
+server.listen(PORT, () => console.log(`🌎  Listening on port ${PORT}`));
